ee82b90d0452b25da817e4943eeccf3d0f272f11
michaelwstuart <mike.w.stuart@gmail.com>
2017-09-12 15:42:08 -0700

    change cors to deployed front end

--- src/server.js
+++ src/server.js
@@ -6,7 +6,7 @@ import routes from './routes';
 const app = express();
 
 app
-  .use(cors({ origin: 'http://localhost:8080', credentials: true }))
+  .use(cors({ origin: 'https://feastech.herokuapp.com', credentials: true }))
   .use(bodyParser.json())
   .use(routes)
   .listen(process.env.PORT || 8080, () => console.log('server on port 8080'));
