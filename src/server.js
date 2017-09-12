b75dd381addc429b7110ed4b15ee3797c76c4913
michaelwstuart <mike.w.stuart@gmail.com>
2017-09-12 15:10:41 -0700

    add cors for localhost

--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 import bodyParser from 'body-parser'
 import routes from './routes';
 
 const app = express();
 
 app
+  .use(cors({ origin: 'http://localhost:8080', credentials: true }))
   .use(bodyParser.json())
   .use(routes)
   .listen(process.env.PORT || 8080, () => console.log('server on port 8080'));
